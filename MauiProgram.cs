159438ccda7d74512af22cca46615ac57129210f
gurdian0614 <ivan.gurdian@outlook.com>
2026-08-31 19:19:11 -0600

    Adding singleton to ProductoService

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using ProductosApp_Q32026.Services;
 
 namespace ProductosApp_Q32026;
 
@@ -14,6 +15,9 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
+
+		// Una sola conexion a la base de datos
+		builder.Services.AddSingleton<ProductoService>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
